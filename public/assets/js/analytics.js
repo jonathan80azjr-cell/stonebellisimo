84030e41dab58939d7146c4ab70b2b92f238fe61
jonathan80azjr-cell <cesaryunda@cesars-mbp.myfiosgateway.com>
2026-07-08 22:31:03 -0400

    Restore Google Analytics tag support

---
+++ public/assets/js/analytics.js
@@ -0,0 +1,25 @@
+(function () {
+  const measurementId = window.STONE_BELLISIMO_GA_ID || 'G-XXXXXXXXXX';
+
+  if (!measurementId || measurementId === 'G-XXXXXXXXXX') {
+    return;
+  }
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', measurementId, {
+    anonymize_ip: true,
+    send_page_view: true
+  });
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+})();
